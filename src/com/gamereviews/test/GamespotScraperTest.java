4c451606eb18e66906b16cd4adc28c2936e69e48
todd-elvers <todd.elvers@gmail.com>
2013-07-16 16:24:17 -0500

    Split the single test encompassing all features into separate tests to add clarification; refactored some variables.
    Previously all features were tested inside the 'testGetReview()' test.  I split that large test up into many small tests to try and make clearer what was being tested.

--- src/com/gamereviews/test/GamespotScraperTest.java
+++ src/com/gamereviews/test/GamespotScraperTest.java
@@ -1,43 +1,78 @@
 package com.gamereviews.test;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.gamereviews.model.GamespotReview;
 import com.gamereviews.scraper.GamespotScraper;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class GamespotScraperTest {
 
-	@Test
-	public void testGetReview() {
-		GamespotScraper gamespotScraper = new GamespotScraper();
-		
-		Assert.assertNull(gamespotScraper.getReview(""));
-		Assert.assertNull(gamespotScraper.getReview(" "));
-		Assert.assertNull(gamespotScraper.getReview("	"));
-		Assert.assertNull(gamespotScraper.getReview(null));
-		
-		// Incorrect Game title
-		Assert.assertNull(gamespotScraper.getReview("12345"));
-		Assert.assertNull(gamespotScraper.getReview("20"));
-		Assert.assertNull(gamespotScraper.getReview("!@#$"));
-		Assert.assertNull(gamespotScraper.getReview("AaaBBBxx122##$@!"));
-		
-		// Correct Game title
-		GamespotReview review;
-		Assert.assertNotNull(review = gamespotScraper.getReview("fifa soccer 13"));
-		Assert.assertNotNull(review.getGameTitle());
-		Assert.assertNotNull(review.getGenre());
-		Assert.assertNotNull(review.getReleaseDate());
-		Assert.assertNotNull(review.getScorePS3());
-		Assert.assertNotNull(review.getScoreXBOX360());
-		Assert.assertNotNull(review.getScorePS_VITA());
-		
-		Assert.assertNotNull(gamespotScraper.getReview("call of duty black ops 2"));
-		Assert.assertNotNull(gamespotScraper.getReview("call of duty black ops II"));
-		Assert.assertNotNull(gamespotScraper.getReview("Portal 2"));
-		Assert.assertNull(gamespotScraper.getReview("Portal II"));
-		Assert.assertNotNull(gamespotScraper.getReview("Temple Run"));
-	}
+    private GamespotScraper gamespotScraper;
+
+    @Before
+    public void setUp() throws Exception {
+        gamespotScraper = new GamespotScraper();
+    }
+
+    @Test
+    public void testThatNullOrEmptyStringRequestsReturnNull() {
+        assertNull(gamespotScraper.getReview(""));
+        assertNull(gamespotScraper.getReview(" "));
+        assertNull(gamespotScraper.getReview("	"));
+        assertNull(gamespotScraper.getReview(null));
+    }
+
+    @Test
+    public void testThatInvalidRequestsReturnNull() {
+        assertNull(gamespotScraper.getReview("12345"));
+        assertNull(gamespotScraper.getReview("20"));
+        assertNull(gamespotScraper.getReview("!@#$"));
+        assertNull(gamespotScraper.getReview("AaaBBBxx122##$@!"));
+    }
+
+    @Test
+    public void testThatValidGameTitlesReturnPopulatedReviews() {
+        GamespotReview review;
+        assertNotNull(review = gamespotScraper.getReview("fifa soccer 13"));
+        assertNotNull(review.getGameTitle());
+        assertNotNull(review.getGenre());
+        assertNotNull(review.getReleaseDate());
+        assertNotNull(review.getScorePS3());
+        assertNotNull(review.getScoreXBOX360());
+        assertNotNull(review.getScorePS_VITA());
+        assertNull(review.getScorePC());
+        assertNull(review.getScore3DS());
+        assertNull(review.getScoreIOS());
+    }
+
+    @Test
+    public void testThatGamesWithTitlesThatContainRomanNumeralsCanBeFoundByUsingRomanNumeralsOrDecimalNumbers() {
+        GamespotReview reviewRetrievedByDecimalNumber = gamespotScraper.getReview("call of duty black ops 2");
+        GamespotReview reviewRetrievedByRomanNumeral = gamespotScraper.getReview("call of duty black ops II");
+
+        assertNotNull(reviewRetrievedByDecimalNumber);
+        assertNotNull(reviewRetrievedByRomanNumeral);
+
+        assertEquals(reviewRetrievedByDecimalNumber.getGameTitle(), reviewRetrievedByRomanNumeral.getGameTitle());
+    }
+
+    @Test
+    public void testThatGamesWithTitlesThatContainDecimalNumbersCanOnlyBeFoundByUsingDecimalNumbers() {
+        GamespotReview reviewRetrievedByDecimalNumber = gamespotScraper.getReview("Portal 2");
+        GamespotReview reviewRetrievedByRomanNumeral = gamespotScraper.getReview("Portal II");
+
+        assertNotNull(reviewRetrievedByDecimalNumber);
+        assertNull(reviewRetrievedByRomanNumeral);
+    }
+
+    @Test
+    public void testThatIOSGamesCanBeScraped() {
+        GamespotReview review = gamespotScraper.getReview("Temple Run");
+        assertNotNull(review);
+    }
 
 }
